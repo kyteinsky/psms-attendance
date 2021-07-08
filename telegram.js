d2b1b3b0015db34cc3037c787c4e190a9428b4c1
kyte <kyteinsky@gmail.com>
2021-07-08 11:08:19 +0530

    safety check for file exists before telegram sendImage

--- telegram.js
+++ telegram.js
@@ -2,6 +2,7 @@
 
 require("dotenv").config();
 const TelegramBot = require('node-telegram-bot-api');
+const fs = require('fs');
 
 const bot = new TelegramBot(
 	process.env.telegramBotApiToken,
@@ -10,9 +11,9 @@ const bot = new TelegramBot(
 
 async function sendImages(chatId) {
 	try {
-		await bot.sendPhoto(chatId, './ss.png', { contentType: 'image' });
-		await bot.sendPhoto(chatId, './ss2.png');
-		await bot.sendPhoto(chatId, './ss2.png');
+		if (fs.existsSync('./ss.png')) await bot.sendPhoto(chatId, './ss.png');
+		if (fs.existsSync('./ss2.png')) await bot.sendPhoto(chatId, './ss2.png');
+		if (fs.existsSync('./ss3.png')) await bot.sendPhoto(chatId, './ss3.png');
 		console.log('images sent');
 	} catch (err) {
 		console.error(err);
